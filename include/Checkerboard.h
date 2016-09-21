e70eb4632cd26e0b83f54adcf7960b8f671fd4b7
Tal Ein-Gar <tal.eingar@gmail.com>
2016-09-21 12:16:58 -0700

    Changed CBoard implementation so the get_color does not need to compute the matrix inverse every time

--- include/Checkerboard.h
+++ include/Checkerboard.h
@@ -6,9 +6,8 @@
 namespace Raytracer148 {
 	class CBoard : public Shape {
 	public:
-		CBoard(Eigen::Vector3d &origin, Eigen::Vector3d &v1, Eigen::Vector3d &v2, Eigen::Vector3d &color1, Eigen::Vector3d &color2, int blocks, double reflectStrength) : 
-			o(origin), u(v1), v(v2), o_color1(color1), o_color2(color2), b(blocks), reflect(reflectStrength) {}
-
+		CBoard(Eigen::Vector3d &origin, Eigen::Vector3d &v1, Eigen::Vector3d &v2, Eigen::Vector3d &color1, Eigen::Vector3d &color2, int blocks, double reflectStrength);
+	
 		virtual HitRecord intersect(const Ray &ray);
 		virtual Eigen::Vector3d getColor(const Eigen::Vector3d &position);
 		virtual double getReflectStrength();
@@ -19,6 +18,8 @@ namespace Raytracer148 {
 		Eigen::Vector3d v;
 		Eigen::Vector3d o_color1;
 		Eigen::Vector3d o_color2;
+		Eigen::Matrix3d m_inv;     // this is the inverse matrix of (o|u|v). Keeping it in memory to avoid multiple calculations of this matrix. Must check value of m_isInvertible to verify validty of value.
+		bool m_isInvertible;       // true if the matrix (o|u|v) is invertible (and then the value of m_inv is meaningful).
 		int b;
 		double reflect;
 	};
--- src/Checkerboard.cpp
+++ src/Checkerboard.cpp
@@ -6,13 +6,23 @@ using namespace Raytracer148;
 using namespace Eigen;
 using namespace std;
 
+CBoard::CBoard(Eigen::Vector3d &origin, Eigen::Vector3d &v1, Eigen::Vector3d &v2, Eigen::Vector3d &color1, Eigen::Vector3d &color2, int blocks, double reflectStrength) :
+	o(origin), u(v1), v(v2), o_color1(color1), o_color2(color2), b(blocks), reflect(reflectStrength)  {
+	Eigen::Matrix3d m;
+	m.col(0) = o;
+	m.col(1) = u;
+	m.col(2) = v;
+	m_isInvertible = (m.determinant() != 0);
+	m_inv = m.inverse();
+}
+
 HitRecord CBoard::intersect(const Ray &ray) {
 	HitRecord result;
 	result.t = -1;
 		
 	// set up intersection at 
 	
-	Eigen::MatrixXd m(3, 3);
+	Eigen::Matrix3d m;
 	m.col(0) = -ray.direction;
 	m.col(1) = u;
 	m.col(2) = v;
@@ -38,11 +48,9 @@ HitRecord CBoard::intersect(const Ray &ray) {
 
 
 Vector3d CBoard::getColor(const Eigen::Vector3d &position) {
-	Eigen::MatrixXd m(3, 3);
-	m.col(0) = o;
-	m.col(1) = u;
-	m.col(2) = v;
-	Eigen::Vector3d res = m.inverse() * position;
+	if (!m_isInvertible)			 // should not reach this point. If m_isInvertible there should be no hit, and therefore no position...)
+		return o_color1;    
+	Eigen::Vector3d res = m_inv * position;
 	if ( ((int)(res[1]*b) + (int)(res[2]*b))%2 )
 		return o_color1;
 	return o_color2;
